a02e1ba82d294576396e512f3ff1cab222fd5ccd
Anton Northern <48867587+koshikishi@users.noreply.github.com>
2019-11-04 15:45:40 +0300

    :bento: Подключил шрифты

--- gulpfile.js
+++ gulpfile.js
@@ -103,7 +103,7 @@ exports.webp = function () {
     .pipe(webp({
       quality: 90
     }))
-    .pipe(dest(`source/img`));
+    .pipe(dest(`build/img`));
 };
 
 // Удаление файлов в папке build перед копированием
